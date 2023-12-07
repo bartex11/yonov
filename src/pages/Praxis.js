b2c4d0d12876ac433d9fd629e12c91d0b961f06a
Ivan <vasilev@engine-productions.de>
2023-12-07 15:45:10 +0100

    some content

--- src/pages/Praxis.js
+++ src/pages/Praxis.js
@@ -16,9 +16,54 @@ export default function Praxis({ data }) {
           <h1 className="page-title">
             {data.praxisTitle}
           </h1>
-        </Col>
-        <Col xs={12}>
+
           <p>{data.praxisParagraph}</p>
+
+          <h2>{data.praxisTitle1}</h2>
+          <p>{data.praxisParagraph1}</p>
+          <ul>
+            <li>{data.li}</li>
+            <li>{data.li1}</li>
+            <li>{data.li2}</li>
+          </ul>
+          <p>{data.praxisParagraph2}</p>
+          <ul>
+            <li>{data.li3}</li>
+            <li>{data.li4}</li>
+          </ul>
+
+          {data.praxisParagraph3
+            ? <p>{data.praxisParagraph3}</p>
+            : ''
+          }
+
+          {data.praxisParagraph4
+            ? <p>{data.praxisParagraph4}</p>
+            : ''
+          }
+
+          {data.praxisParagraph5
+            ? <p>{data.praxisParagraph5}</p>
+            : ''
+          }
+
+          {data.praxisParagraph6
+            ? <p>{data.praxisParagraph6}</p>
+            : ''
+          }
+
+          {data.praxisParagraph7
+            ? <p>{data.praxisParagraph7}</p>
+            : ''
+          }
+          {data.praxisParagraph8
+            ? <p>{data.praxisParagraph8}</p>
+            : ''
+          }
+
+          <h2>{data.praxisTitle2}</h2>
+
+
         </Col>
 
        
